65e4c29b23867d81c2fc86aaea56813f7388f688
4vrenim <58933207+4vrenim@users.noreply.github.com>
2025-02-18 16:29:33 +0700

    Update main.tf

--- lab11/main.tf
+++ lab11/main.tf
@@ -59,7 +59,7 @@ resource "azurerm_lb_rule" "lb_rule" {
   name                           = "http-rule"
   loadbalancer_id                = azurerm_lb.lb.id
   frontend_ip_configuration_name = "PublicIP"
-  backend_address_pool_ids       = azurerm_lb_backend_address_pool.lb_backend.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.lb_backend.id]
   protocol                       = "Tcp"
   frontend_port                  = 80
   backend_port                   = 80
